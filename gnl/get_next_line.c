326bd0fbd3435aa39a19a15dbf50f72cd248d7a8
Venom <vjacob@ev-o3.kzn.21-school.ru>
2021-01-03 14:30:51 +0300

    just

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,11 +6,31 @@
 /*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 15:13:35 by vjacob            #+#    #+#             */
-/*   Updated: 2020/12/10 15:13:43 by vjacob           ###   ########.fr       */
+/*   Updated: 2021/01/03 13:32:24 by vjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int get_next_line(int fd, char **line)
 {
 	
+}
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main()
+{
+	int		fd = open("file", O_RDONLY);
+	char	*line;
+	int 	i;
+	while (i = get_next_line(fd, &line))
+	{
+		printf("i = %d %s\n", i, line);
+		free(line);
+	}
+	printf("i = %d %s\n", i, line);
+	free(line);
+	return (0);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,10 +6,15 @@
 /*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 15:14:03 by vjacob            #+#    #+#             */
-/*   Updated: 2020/12/10 15:14:27 by vjacob           ###   ########.fr       */
+/*   Updated: 2021/01/03 13:24:32 by vjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFFER_SIZE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <unistd.h>
+#include <stdlib.h>
 
-int get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
+
+#endif
