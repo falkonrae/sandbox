9feb92840e61ff26f1a5fb74fac175435f59008f
Venom <vjacob@ev-o3.kzn.21-school.ru>
2021-01-03 16:05:07 +0300

    gddnl

---
+++ gnl/get_next_line_bonus_dd.h
@@ -0,0 +1,17 @@
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
