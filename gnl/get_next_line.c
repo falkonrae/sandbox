4c85d1443685d00a9a56d87a6a84d8c065e03283
Venom <vjacob@ev-o3.kzn.21-school.ru>
2021-01-19 13:19:59 +0300

    changes??

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,11 +6,26 @@
 /*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 15:13:35 by vjacob            #+#    #+#             */
-/*   Updated: 2021/01/15 17:08:19 by vjacob           ###   ########.fr       */
+/*   Updated: 2021/01/19 13:15:37 by vjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int free_all(char **buf, char **line)
+{
+	if (*buf != NULL)
+	{
+		free(*buf);
+		*buf = NULL;
+	}
+	if (*line != NULL)
+	{
+		free(*line);
+		*line = NULL;
+	}
+	return (-1);
+}
 
 char *check_ost(char	*ost, char **line)
 {
@@ -23,19 +38,16 @@ char *check_ost(char	*ost, char **line)
 		{
 			*symb = '\0';
 			*line = ft_strdup(ost);
+			symb++;
 			ft_strcpy(ost, ++symb);
 		}
 		else
-		{
 			*line = ft_strdup(ost);
-			ft_strclr(ost);
-		}
 	}
 	else
-		*line = ft_new(1);
+		*line = ft_strdup("");
 	return (symb);
 }
-
 
 int get_next_line(int fd, char **line)
 {
@@ -43,24 +55,30 @@ int get_next_line(int fd, char **line)
 	char 		*buf;
 	char 		*symb;
 	int 		readed_byte;
+
 	if (!line|| fd < 0 || BUFFER_SIZE <= 0)
 	 	return (-1);
 	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-		return (-1);
+		return (free_all(&buf, line));
 	symb = check_ost(ost, line);
 	while (!symb && ((readed_byte = read(fd, buf, BUFFER_SIZE))))
 	{
+		if (readed_byte < 0)
+			return (free_all(&ost, &buf));
+		*symb = '\0';
 		if ((symb = ft_strchr(buf, '\n')))
 		{
 			*symb = '\0';
 			symb++;
+			free(ost);
 			ost = ft_strdup(symb);
 		}
-		buf[BUFFER_SIZE] = '\0';
 		*line = ft_strjoin(*line, buf);
 	}
-	
-	return (0);
+	free (buf);
+	if (readed_byte == 0 && ost != 0)
+		free_all(&ost, &ost);
+	return (readed_byte || symb) ? 1 : 0;
 }
 
 
--- gnl/get_next_line_dd.c
+++ gnl/get_next_line_dd.c
@@ -1,6 +1,4 @@
-************************************************************************** */
-
-#include "get_next_line.h"
+/*#include "get_next_line.h"
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -34,7 +32,7 @@ char	*ft_save(char *buf, char *ost)
 	return (ost);
 }
 
-char	*ft_check_line(char *ost, char **line, int resread)
+char	*ft_check_line(char *ost, char **line, int readed_byte)
 {
 	unsigned int	i;
 	char			*copy;
@@ -54,7 +52,7 @@ char	*ft_check_line(char *ost, char **line, int resread)
 		ost = ft_strdup(copy);
 		free(copy);
 	}
-	else if (resread == 0)
+	else if (readed_byte == 0)
 	{
 		*line = ost;
 		ost = NULL;
@@ -62,49 +60,130 @@ char	*ft_check_line(char *ost, char **line, int resread)
 	return (ost);
 }
 
-int		retresread(char **ost, int fd)
+int		retreaded_byte(char **ost, int fd)
 {
 	char	*buf;
-	int		resread;
+	int		readed_byte;
 
 	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
-	while (!ft_strchr(*ost, '\n') && (resread = read(fd, buf, BUFFER_SIZE)))
+	while (!ft_strchr(*ost, '\n') && (readed_byte = read(fd, buf, BUFFER_SIZE)))
 	{
-		if (resread == -1)
+		if (readed_byte == -1)
 		{
 			free(buf);
 			buf = NULL;
 			return (-1);
 		}
-		buf[resread] = '\0';
+		buf[readed_byte] = '\0';
 		*ost = ft_save(buf, *ost);
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
 	free(buf);
 	buf = NULL;
-	return (resread);
+	return (readed_byte);
 }
 
 int		get_next_line(int fd, char **line)
 {
 	static char	*ost[4096];
-	int			resread;
+	int			readed_byte;
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
 	if (!ost[fd] && !(ost[fd] = ft_strdup("")))
 		return (-1);
-	if ((resread = retresread(&ost[fd], fd)) == -1)
+	if ((readed_byte = retreaded_byte(&ost[fd], fd)) == -1)
 		return (-1);
-	if (resread <= 0 && !ost[fd])
+	if (readed_byte <= 0 && !ost[fd])
 	{
 		*line = ft_strdup("");
-		return (resread);
+		return (readed_byte);
 	}
-	ost[fd] = ft_check_line(ost[fd], line, resread);
-	if (resread <= 0 && !ost[fd])
-		return (resread);
+	ost[fd] = ft_check_line(ost[fd], line, readed_byte);
+	if (readed_byte <= 0 && !ost[fd])
+		return (readed_byte);
 	return (1);
+}
+
+*/
+
+#include "get_next_line.h"
+
+int					memory_free(char **s, char **s2)
+{
+	if (*s)
+	{
+		free(*s);
+		*s = NULL;
+	}
+	if (*s2)
+	{
+		free(*s2);
+		*s2 = NULL;
+	}
+	return (-1);
+}
+
+char				*chk_ost(char *ost, char **line)
+{
+	char			*nline;
+
+	nline = NULL;
+	if (ost)
+	{
+		if ((nline = ft_strchr(ost, '\n')))
+		{
+			*nline = '\0';
+			*line = ft_strdup(ost);
+			nline++;
+			ft_strcpy(ost, nline);
+		}
+		else
+			*line = ft_strdup(ost);
+	}
+	else
+		*line = ft_strdup("");
+	return (nline);
+}
+
+void				do_if(char **buf, char **nline, char **ost)
+{
+	if ((*nline = ft_strchr(*buf, '\n')))
+	{
+		**nline = '\0';
+		(*nline)++;
+		free(*ost);
+		*ost = ft_strdup(*nline);
+	}
+}
+
+int					get_next_line(int fd, char **line)
+{
+	static char		*ost;
+	char			*buf;
+	char			*nline;
+	int				n;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || !line)
+		return (-1);
+	n = 1;
+	nline = chk_ost(ost, line);
+	if (!(buf = malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (memory_free(&buf, line));
+	while (!nline && (n = read(fd, buf, BUFFER_SIZE)))
+	{
+		if (n < 0)
+		{
+			return (memory_free(&ost, &buf));
+		}
+		buf[n] = '\0';
+		do_if(&buf, &nline, &ost);
+		*line = ft_strjoin(*line, buf);
+	}
+	free(buf);
+	if (n == 0 && ost != 0)
+		memory_free(&ost, &ost);
+	return (n || nline) ? 1 : 0;
 }
